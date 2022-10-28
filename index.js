b8ba0656f2e35d6824f54b4b58a3911b7228d31e
junaaid96 <info.junaidulislam@gmail.com>
2022-10-28 16:44:21 +0600

    all courses loaded

--- index.js
+++ index.js
@@ -3,13 +3,27 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courseCategories = require('./data/course_categories.json');
+const courses = require('./data/courses.json');
+
 app.use(cors());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Learn-Tech-Edge API Running...')
 });
 
-app.listen(port, ()=>{
-    console.log(`Learn-Tech-Edge Server is running at port: ${port}`);
+app.get('/course-categories', (req, res) => {
+    res.send(courseCategories);
 });
 
+
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Learn-Tech-Edge Server is running at port: ${port}`);
+});
